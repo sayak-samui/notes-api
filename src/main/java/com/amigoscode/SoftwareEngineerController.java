1b0af04c962f1ebbdbe48ee9e0d8182247286240
sayak-samui <sayaksamui1@gmail.com>
2026-06-18 16:52:51 +0530

    Add software engineer REST API

---
+++ src/main/java/com/amigoscode/SoftwareEngineerController.java
@@ -0,0 +1,29 @@
+package com.amigoscode;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers(){
+        return List.of(
+                new SoftwareEngineer(
+                        1,
+                        "james",
+                        "js,node, react, tailwind"
+                ),
+                new SoftwareEngineer(
+                        2,
+                        "jamila",
+                        "java, spring, spring boot"
+                )
+
+        );
+    }
+}
